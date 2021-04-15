7ffdbbfbadaa720e17803cf8854955362fbcd15f
xalys_ura <xalysjyra1992@gmail.com>
2021-04-15 20:53:07 +0300

    Добавлено скрапт для історій і добавлено нові карточки і також карточки в більше

--- script/main.js
+++ script/main.js
@@ -1,9 +1,17 @@
+const buttonMoreHistory = document.querySelector(".button_history");
+const blockMoreHistory = document.querySelector(".more_history");
 const buttonMoreNow = document.querySelector(".more");
 const blockCardMore = document.querySelector(".more_card");
 
 // buttonMoreNow.addEventListener('click', function () {
 //    blockCardMore.classList.toggle('add')
 // })
+buttonMoreHistory.addEventListener("click", moreHistory);
+
+function moreHistory() {
+   blockMoreHistory.classList.toggle("add");
+   buttonMoreHistory.classList.toggle("none");
+}
 
 buttonMoreNow.addEventListener("click", toggleModal);
 
