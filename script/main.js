40c09b5c53e0c189a6848d8d8a99ea3ec291bb7b
xalys_ura <xalysjyra1992@gmail.com>
2021-04-19 23:27:40 +0300

    Все зроблене за понеділок а саме добавлено 2 історії зроблене модальне вікно і оптимізоване для телефону і скрипти для відкривання

--- script/main.js
+++ script/main.js
@@ -3,10 +3,24 @@ const blockMoreHistory = document.querySelector(".more_history");
 const buttonMoreNow = document.querySelector(".more2");
 const buttonLess = document.querySelector(".less");
 const blockCardMore = document.querySelector(".more_card");
+const card = document.querySelector(".card_itm");
+const modalCardBlock = document.querySelector(".modal");
+const close = document.querySelector(".close");
 
 // buttonMoreNow.addEventListener('click', function () {
 //    blockCardMore.classList.toggle('add')
 // })
+card.addEventListener("click", modalCard);
+
+function modalCard() {
+   modalCardBlock.classList.toggle("add");
+}
+
+close.addEventListener("click", closef);
+function closef() {
+   modalCardBlock.classList.toggle("add");
+}
+
 buttonMoreHistory.addEventListener("click", moreHistory);
 
 function moreHistory() {
