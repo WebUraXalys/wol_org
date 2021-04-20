2b0dd5ed4e3a4833613e6e3a5e8ee3eefd62ba57
xalys_ura <xalysjyra1992@gmail.com>
2021-04-20 19:08:00 +0300

    Зроблино вивід різних карточок

--- script/main.js
+++ script/main.js
@@ -3,22 +3,38 @@ const blockMoreHistory = document.querySelector(".more_history");
 const buttonMoreNow = document.querySelector(".more2");
 const buttonLess = document.querySelector(".less");
 const blockCardMore = document.querySelector(".more_card");
-const card = document.querySelector(".card_itm");
-const modalCardBlock = document.querySelector(".modal");
-const close = document.querySelector(".close");
+const cards = document.querySelectorAll(".card_itm");
+const modalCardBlock = document.querySelectorAll(".modal");
+const close = document.querySelectorAll(".close");
 
+console.log(cards);
 // buttonMoreNow.addEventListener('click', function () {
 //    blockCardMore.classList.toggle('add')
 // })
-card.addEventListener("click", modalCard);
 
-function modalCard() {
-   modalCardBlock.classList.toggle("add");
+// cards[0].addEventListener("click", modalCard);
+
+// function modalCard() {
+//    modalCardBlock.classList.toggle("add");
+// }
+
+// Пробую написати цикл щоб працювиля всі карточки
+
+// for (let i = 0; i < 3; i++) { 
+//    alert(i);
+// }
+for (let i = 0; i < 4; i++) {
+   cards[i].addEventListener("click", modalCard);
+
+   function modalCard() {
+      modalCardBlock[i].classList.toggle("add");
+   }  
 }
-
-close.addEventListener("click", closef);
-function closef() {
-   modalCardBlock.classList.toggle("add");
+for (let i = 0; i < 4; i++){
+   close[i].addEventListener("click", closef);
+   function closef() {
+      modalCardBlock[i].classList.toggle("add");
+   }
 }
 
 buttonMoreHistory.addEventListener("click", moreHistory);
