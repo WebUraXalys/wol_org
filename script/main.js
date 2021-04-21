9f0192ad3e9f7e8fbbc26f62e3b44255ec387bd7
xalys_ura <xalysjyra1992@gmail.com>
2021-04-21 23:06:01 +0300

    Зроблено кнопки історій а також і модальне вікно дороблене

--- script/main.js
+++ script/main.js
@@ -1,28 +1,16 @@
 const buttonMoreHistory = document.querySelector(".button_history");
-const blockMoreHistory = document.querySelector(".more_history");
+const blockMoreHistory = document.querySelectorAll(".more_history");
+const buttonHistory = document.querySelectorAll(".button_history");
+const buttonHistoryLess = document.querySelectorAll(".button_history_less");
+const buttonHistoryMore = document.querySelectorAll(".button_history_more");
 const buttonMoreNow = document.querySelector(".more2");
-const buttonLess = document.querySelector(".less");
 const blockCardMore = document.querySelector(".more_card");
+const buttonLess = document.querySelector(".less");
 const cards = document.querySelectorAll(".card_itm");
 const modalCardBlock = document.querySelectorAll(".modal");
 const close = document.querySelectorAll(".close");
 
-console.log(cards);
-// buttonMoreNow.addEventListener('click', function () {
-//    blockCardMore.classList.toggle('add')
-// })
 
-// cards[0].addEventListener("click", modalCard);
-
-// function modalCard() {
-//    modalCardBlock.classList.toggle("add");
-// }
-
-// Пробую написати цикл щоб працювиля всі карточки
-
-// for (let i = 0; i < 3; i++) { 
-//    alert(i);
-// }
 for (let i = 0; i < 4; i++) {
    cards[i].addEventListener("click", modalCard);
 
@@ -37,11 +25,40 @@ for (let i = 0; i < 4; i++){
    }
 }
 
+// buttonMoreHistory.addEventListener("click", moreHistory);
+
+// function moreHistory() {
+//    blockMoreHistory[0].classList.add("add");
+//    buttonMoreHistory.classList.toggle("none");
+// }
+
+// buttonHistoryMore.addEventListener("click", moreHistory);
+
 buttonMoreHistory.addEventListener("click", moreHistory);
 
 function moreHistory() {
-   blockMoreHistory.classList.toggle("add");
-   buttonMoreHistory.classList.toggle("none");
+      blockMoreHistory[0].classList.toggle("add");
+      buttonMoreHistory.classList.toggle("none");
+   }
+for (let i = 0; i < 2; i++){
+   buttonHistoryMore[i].addEventListener("click", moreHistory2);
+   function moreHistory2() {
+      blockMoreHistory[i+1].classList.add("add");
+      buttonHistoryMore[i].classList.toggle("none");
+      // buttonHistory[i+1].classList.add("none");
+   }
+
+}
+
+
+ for (let i = 0; i < 3; i++){ //Модливо вдасця зробити закриття зразу всіх історій
+   buttonHistoryLess[i].addEventListener("click", lessHistory);
+   console.log(i);
+   function lessHistory() {
+      blockMoreHistory[i].classList.remove("add");
+      console.log(i+"два");
+      buttonMoreHistory.classList.toggle("none");
+   }
 }
 
 buttonMoreNow.addEventListener("click", addCard);
